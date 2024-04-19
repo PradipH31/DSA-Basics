ba332d20f9240b707b769f15d0194fc0d61067cd
PradipH31 <pradip.hamal31@gmail.com>
2024-04-19 15:21:54 -0500

    Added queue and stack DS

---
+++ LinkedList/Queue/Queue.py
@@ -0,0 +1,12 @@
+stack_example = []
+stack_example.append(1)
+stack_example.append(23)
+stack_example.append(32)
+stack_example.append(78)
+stack_example.append(99)
+stack_example.append(100)
+
+print(stack_example)
+
+print(stack_example.pop(0))
+print(stack_example)
